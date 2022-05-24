b8bdedb7a7156e6e62c9586555546292cf622ceb
White Nguyen <whitenguyen0909@gmail.com>
2022-05-24 17:30:38 -0600

    fixed the path for the imported files, added validateZip function,  added a route in app.js to check zipcode

--- src/app.js
+++ src/app.js
@@ -4,13 +4,21 @@ const morgan = require("morgan");
 const app = express();
 
 // Middleware
-const validateZip = require("middleware/validateZip.js");
-const getZoos = require("utils/getZoos.js");
+const validateZip = require("./middleware/validateZip.js");
+const getZoos = require("./utils/getZoos.js");
 
 // Application-level middleware
 app.use(morgan("dev"));
 
 // Routes
+app.get("/check/:zip", validateZip, (req, res) => {
+  const zipcode = req.params.zip;
+  if (getZoos(zipcode)) {
+    res.send(`${zipcode} exists in our records.`);
+  } else {
+    res.send(`${zipcode} does not exist in our record.`);
+  }
+});
 
 // No-route handler
 app.use((req, res, next) => {
--- src/middleware/validateZip.js
+++ src/middleware/validateZip.js
@@ -1,3 +1,10 @@
-function validateZip(req, res, next) {}
+function validateZip(req, res, next) {
+  const zipcode = req.params.zip;
+  if (zipcode.length === 5 && Number.isInteger(parseInt(zipcode))) {
+    next();
+  } else {
+    next(`Zip (${zipcode}) is invalid`);
+  }
+}
 
 module.exports = validateZip;
