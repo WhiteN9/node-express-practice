4fe0a0eb90a1bb356175e7805822c548a5c97d07
White Nguyen <whitenguyen0909@gmail.com>
2022-05-24 17:18:05 -0600

    added imports in app and server, added app.listen to server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+// Middleware
+
+// Application-level middleware
+
+// Routes
+
+// No-route handler
+
+// Error handler
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,6 @@
+const { PORT = 8080 } = process.env;
+
+const app = require("./app");
+
+const listener = () => (console.log(`Hello, we are at ${PORT} port`));
+app.listen(PORT, listener)
